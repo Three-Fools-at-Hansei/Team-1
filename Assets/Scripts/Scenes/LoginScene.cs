0dca4c4e3a3d0ca0b64c87fb81f0ee8600c52236
승윤 <tmdbs6781@naver.com>
2025-11-08 20:16:30 +0900

    feat: LoginScene.cs 추가

---
+++ Assets/Scripts/Scenes/LoginScene.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+using System.Threading.Tasks;
+
+public class LoginScene : MonoBehaviour, IScene
+{
+    eSceneType IScene.SceneType => eSceneType.Login;
+
+    private bool _initialized;
+
+    void Awake()
+    {
+        Managers.Scene.SetCurrentScene(this);
+    }
+
+    async void Start()
+    {
+        await InitializeAsync();
+    }
+
+    async void IScene.Init()
+    {
+        await InitializeAsync();
+    }
+
+    private async Task InitializeAsync()
+    {
+        if (_initialized)
+            return;
+
+        _initialized = true;
+
+        await EnsureLoginServiceAsync();
+
+        Debug.Log("LoginScene Init");
+        await Managers.UI.ShowAsync<UI_LoginView>(new LoginViewModel());
+    }
+
+    private async Task EnsureLoginServiceAsync()
+    {
+        if (Login.Instance == null)
+        {
+            var loginGo = new GameObject("@Login");
+            loginGo.AddComponent<Login>();
+        }
+
+        await Login.Instance.EnsureInitializedAsync();
+    }
+
+    void IScene.Clear()
+    {
+        Debug.Log("LoginScene Clear");
+    }
+}
