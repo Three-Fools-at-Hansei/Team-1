e5420dc6ab069580156eb2db6744b820db30c13c
승윤 <tmdbs6781@naver.com>
2025-11-13 19:13:13 +0900

    feat:회원가입 기능 추가 ILoginView.cs 수정

--- Assets/Scripts/UI/IView/ILoginView.cs
+++ Assets/Scripts/UI/IView/ILoginView.cs
@@ -4,8 +4,14 @@ public interface ILoginView
 {
     void SetIdInputText(string text);
     void SetPasswordInputText(string text);
+    void SetSignupNicknameText(string text);
+    void SetSignupIdText(string text);
+    void SetSignupPasswordText(string text);
+    void SetSignupPasswordConfirmText(string text);
     void SetHelperText(string text);
     void SetSubmitButtonEnabled(bool enabled);
     void SetSubmitButtonColor(Color color);
     void SetTopIconSprite(Sprite sprite);
+    void ShowLoginLayout();
+    void ShowSignupLayout();
 }
