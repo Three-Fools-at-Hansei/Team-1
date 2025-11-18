f473883b99dbbb17b7d9f08440eb54d569ef6ceb
superozing <amazingozing@gmail.com>
2025-11-18 19:18:03 +0900

    feat: 임시 state 추가

--- Assets/Scripts/Scenes/TestScene.cs
+++ Assets/Scripts/Scenes/TestScene.cs
@@ -4,47 +4,42 @@ using UnityEngine;
 public class TestScene : MonoBehaviour, IScene
 {
     eSceneType IScene.SceneType => eSceneType.Test;
-    public List<string> RequiredDataFiles => new() 
-    { 
-        "NikkeGameData.json", 
+    public List<string> RequiredDataFiles => new()
+    {
+        "NikkeGameData.json",
         "ItemGameData.json",
         "MissionGameData.json",
     };
 
-    UI_TabGroupPopup popup;
-
     void Awake()
     {
         Managers.Scene.SetCurrentScene(this);
-        Debug.Log("Test Scene Awake() ÇÕ´Ï´Ù.");
+        Debug.Log("Test Scene Awake() 합니다.");
     }
 
     void IScene.Init()
     {
-        Debug.Log("Test Scene Init() ÇÕ´Ï´Ù.");
+        Debug.Log("Test Scene Init() 합니다.");
         Debug.Log($"persistentDataPath: {Application.persistentDataPath}");
 
         ShowTestUI();
     }
-    
+
     private async void ShowTestUI()
     {
-
-        Debug.Log("Test Scene Init() - Player¿Í NPC¸¦ »ý¼ºÇÕ´Ï´Ù.");
+        Debug.Log("Test Scene Init() - Player와 NPC를 생성합니다.");
 
         Managers.Input.SwitchActionMap("Lobby");
 
-        // ±ÔÁ¤ #3, #7 ÁØ¼ö: Manager¸¦ ÅëÇØ ¾îµå·¹½ººí ÁÖ¼Ò·Î ÇÁ¸®ÆÕÀ» »ý¼ºÇÕ´Ï´Ù.
-
-        // 1. "Player" ÁÖ¼Ò¸¦ °¡Áø Player ÇÁ¸®ÆÕ »ý¼º
+        // 1. "Player" 주소를 가진 Player 프리팹 생성
         GameObject playerGo = await Managers.Resource.InstantiateAsync("Player");
         if (playerGo != null)
         {
-            playerGo.transform.position = Vector3.zero; // »ý¼º ÈÄ À§Ä¡¸¦ (0,0,0)À¸·Î ¼³Á¤
+            playerGo.transform.position = Vector3.zero; // 생성 후 위치를 (0,0,0)으로 설정
         }
 
-        // 2. "BeginnerNPC" ÁÖ¼Ò¸¦ °¡Áø NPC ÇÁ¸®ÆÕ »ý¼º
-        // »ý¼º°ú µ¿½Ã¿¡ À§Ä¡¸¦ (3,0,0)À¸·Î ¼³Á¤
+        // 2. "BeginnerNPC" 주소를 가진 NPC 프리팹 생성
+        // 생성과 동시에 위치를 (3,0,0)으로 설정
         await Managers.Resource.InstantiateAsync("BeginnerNPC", new Vector3(3, 0, 0));
 
         await Managers.UI.ShowAsync<UI_PopupHello>(new HelloPopupViewModel());
@@ -52,8 +47,6 @@ public class TestScene : MonoBehaviour, IScene
 
     void IScene.Clear()
     {
-        //Debug.Log("Test Scene Clear() ÇÕ´Ï´Ù.");
-
-        Managers.UI.Close(popup);
+        //Debug.Log("Test Scene Clear() 합니다.");
     }
 }
