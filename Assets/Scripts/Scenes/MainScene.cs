af49128c5d26f81314b7b93ce7074c83b8cc795c
superozing <amazingozing@gmail.com>
2025-11-26 05:39:17 +0900

    feat: 임시 메인 로비 UI 생성 코드 추가, RequiredDataFiles 목록 추가

--- Assets/Scripts/Scenes/MainScene.cs
+++ Assets/Scripts/Scenes/MainScene.cs
@@ -3,22 +3,30 @@ using UnityEngine;
 
 public class MainScene : MonoBehaviour, IScene
 {
-    eSceneType IScene.SceneType => eSceneType.Main;
-    public List<string> RequiredDataFiles => new List<string>();
+    eSceneType IScene.SceneType => eSceneType.MainScene;
+    public List<string> RequiredDataFiles => new List<string>()
+    {
+        "NikkeGameData.json",
+        "ItemGameData.json",
+        "MissionGameData.json",
+    };
 
     void Awake()
     {
         Managers.Scene.SetCurrentScene(this);
         Debug.Log("Main Scene Awake() 합니다.");
     }
-    void IScene.Init()
+
+    async void IScene.Init()
     {
         Debug.Log("Main Scene Init() 합니다.");
+
+        // 메인 로비 UI 표시
+        await Managers.UI.ShowAsync<UI_MainLobby>(new MainLobbyViewModel());
     }
 
     void IScene.Clear()
     {
         Debug.Log("Main Scene Clear() 합니다.");
     }
-
 }
