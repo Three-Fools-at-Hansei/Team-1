ec81ac021e69be8fe59a559e80b06bb3dfcc35b8
superozing <amazingozing@gmail.com>
2025-12-11 20:43:09 +0900

    feat: Entity Hp ViewModel 추가
    체력바 UI에서 값 대입으로 간단하게 사용할 수 있도록 관련 정보를 변환해 제공하는 뷰모델이에요.

---
+++ Assets/Scripts/UI/ViewModel/EntityHpViewModel.cs
@@ -0,0 +1,47 @@
+using System;
+using UI;
+using UnityEngine;
+
+public class EntityHpViewModel : IViewModel
+{
+    public event Action OnStateChanged;
+
+    private readonly Entity _entity;
+    public string Name { get; private set; }
+
+    public float HpRatio => _entity != null && _entity.MaxHp > 0
+        ? (float)_entity.Hp / _entity.MaxHp
+        : 0f;
+
+    public string HpText => _entity != null
+        ? $"{_entity.Hp} / {_entity.MaxHp}"
+        : "0 / 0";
+
+    public EntityHpViewModel(Entity entity, string nameOverride = null)
+    {
+        _entity = entity;
+        Name = nameOverride ?? _entity.name;
+
+        // 체력 변경 감지
+        if (_entity != null)
+        {
+            _entity.NetHp.OnValueChanged += OnHpChanged;
+            _entity.NetMaxHp.OnValueChanged += OnHpChanged;
+        }
+    }
+
+    private void OnHpChanged(int prev, int cur)
+    {
+        OnStateChanged?.Invoke();
+    }
+
+    // ViewModel이 해제될 때(View에서 연결 끊길 때 등) 필요하다면 호출
+    public void Dispose()
+    {
+        if (_entity != null)
+        {
+            _entity.NetHp.OnValueChanged -= OnHpChanged;
+            _entity.NetMaxHp.OnValueChanged -= OnHpChanged;
+        }
+    }
+}
