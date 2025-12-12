6c52064b401b7b5407148c4bb31a200f5393d999
superozing <amazingozing@gmail.com>
2025-12-12 18:59:10 +0900

    feat: 게임 참가 시 방 번호 입력 방식 사용

--- Assets/Scripts/UI/ViewModel/PlayPopupViewModel.cs
+++ Assets/Scripts/UI/ViewModel/PlayPopupViewModel.cs
@@ -10,27 +10,16 @@ public class PlayPopupViewModel : ViewModelBase
 {
     public override event Action OnStateChanged;
 
-    // View가 구독할 이벤트 정의 (매개변수로 다음 팝업의 ViewModel을 전달)
     public event Action<GameStartConfirmPopupViewModel> OnCreateGamePopupRequested;
+    public event Action<GameJoinPopupViewModel> OnJoinGamePopupRequested;
 
     /// <summary>
     /// 게임 참가 (Quick Join)
     /// </summary>
-    public async void JoinGame()
+    public void JoinGame()
     {
         Debug.Log("[PlayPopupViewModel] 게임 참가(QuickJoin) 요청");
-
-        // 빠른 참가 시도
-        bool success = await Managers.Network.QuickJoinAsync();
-
-        if (success)
-        {
-            Debug.Log("[PlayPopupViewModel] 참가 성공! 호스트의 씬으로 이동 대기 중...");
-        }
-        else
-        {
-            Debug.LogWarning("[PlayPopupViewModel] 참가 실패.");
-        }
+        OnJoinGamePopupRequested?.Invoke(new GameJoinPopupViewModel());
     }
 
     /// <summary>
@@ -39,12 +28,6 @@ public class PlayPopupViewModel : ViewModelBase
     public void ShowCreateGamePopup()
     {
         Debug.Log("[PlayPopupViewModel] 게임 생성 확인 팝업 요청 이벤트 발생");
-
-        // 새로운 ViewModel을 생성하여 이벤트와 함께 전달
-        // ViewModel은 View(UI_GameStartConfirmPopup)를 몰라도 됩니다.
-        var confirmVm = new GameStartConfirmPopupViewModel();
-
-        // View에게 "팝업을 띄워달라"고 요청
-        OnCreateGamePopupRequested?.Invoke(confirmVm);
+        OnCreateGamePopupRequested?.Invoke(new GameStartConfirmPopupViewModel());
     }
 }
